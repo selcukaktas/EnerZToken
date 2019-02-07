d966b1012c4fabd782099460683fa6a88d7d7380
selcukaktas <selcuk.aktas1@gmail.com>
2019-02-07 17:58:41 +0300

    Minor changes.

--- contracts/enerZToken.sol
+++ contracts/enerZToken.sol
@@ -1,18 +1,24 @@
 pragma solidity ^0.5.0;
 
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-
-contract EnerZToken is ERC20Mintable, ERC20Burnable, ERC20Pausable, ERC20Detailed {
+contract EnerZToken is ERC20, ERC20Mintable, ERC20Burnable, ERC20Pausable, ERC20Detailed {
     constructor(
         string memory _name, 
         string memory _symbol, 
         uint8 _decimals
+        //address[] memory minters
         ) 
-    ERC20Detailed(_name, _symbol, _decimals) public 
+        ERC20Burnable()
+        ERC20Mintable()
+        ERC20Pausable()
+        ERC20Detailed(_name, _symbol, _decimals)  
+        ERC20()
+        public
     {
         
     }
